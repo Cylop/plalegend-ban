f5100f7d769f40e5585aedcdfd4ae61b6946f5bb
Nicholas <nicholas.petrasek@gmail.com>
2021-12-21 04:37:10 +0100

    feat: re-wrote ban command

--- bans-plugin/src/main/java/at/nipe/playlegend/playlegendbans/commands/BanCommand.java
+++ bans-plugin/src/main/java/at/nipe/playlegend/playlegendbans/commands/BanCommand.java
@@ -1,96 +1,109 @@
 package at.nipe.playlegend.playlegendbans.commands;
 
-import at.nipe.playlegend.playlegendbans.PlaylegendBansPlugin;
-import at.nipe.playlegend.playlegendbans.entities.Ban;
-import at.nipe.playlegend.playlegendbans.entities.User;
-import at.nipe.playlegend.playlegendbans.exceptions.UnknownDurationUnitException;
+import at.nipe.playlegend.playlegendbans.BanFacade;
+import at.nipe.playlegend.playlegendbans.context.Context;
+import at.nipe.playlegend.playlegendbans.context.ContextProperties;
+import at.nipe.playlegend.playlegendbans.context.LocalePlaceholderHelper;
+import at.nipe.playlegend.playlegendbans.localization.LocalHelper;
+import at.nipe.playlegend.playlegendbans.localization.LocalKeys;
+import at.nipe.playlegend.playlegendbans.localization.LocalizationContainer;
+import at.nipe.playlegend.playlegendbans.shared.resolution.Component;
+import at.nipe.playlegend.playlegendbans.shared.exceptions.UnknownDurationUnitException;
 import at.nipe.playlegend.playlegendbans.parser.durationparser.DurationParser;
-import at.nipe.playlegend.playlegendbans.parser.durationparser.DurationPossibilities;
-import at.nipe.playlegend.playlegendbans.services.bans.BanService;
-import at.nipe.playlegend.playlegendbans.services.users.UserService;
-import dev.alangomes.springspigot.context.Context;
+import at.nipe.playlegend.playlegendbans.shared.utils.Pair;
 import lombok.extern.java.Log;
 import org.bukkit.Bukkit;
-import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
-import picocli.CommandLine;
+import org.bukkit.command.ConsoleCommandSender;
 
+import javax.inject.Inject;
+import java.sql.SQLException;
+import java.util.Arrays;
 import java.util.Date;
-import java.util.concurrent.Callable;
+import java.util.ResourceBundle;
+import java.util.logging.Level;
 
 @Log
-//@Component
-//@CommandLine.Command(name = "legendban", description = "Bans player defined in arguments (index0) with duration (index1) and message (index 2..n)")
-public class BanCommand implements Callable<String> {
-
-    @CommandLine.Parameters(index = "0", description = "The player that should be banned")
-    private String player;
-
-    @CommandLine.Parameters(index = "1", defaultValue = "999y", description = "The duration for ex.: 4w10d3h42s")
-    private String duration;
-
-    @CommandLine.Parameters(index = "2", arity = "1..*", defaultValue = "You have been banned", description = "Ban message or reason")
-    private String message;
+@Component
+public class BanCommand implements CommandExecutor {
 
     private final Context context;
+    private final ResourceBundle resourceBundle;
+    private final BanFacade banFacade;
 
-    private final UserService userService;
-
-    private final BanService banService;
-
-    @Autowired
-    public BanCommand(Context context, UserService userService, BanService banService) {
+    @Inject
+    public BanCommand(Context context, LocalizationContainer localizationContainer, BanFacade banFacade) {
         this.context = context;
-        this.userService = userService;
-        this.banService = banService;
+        this.resourceBundle = localizationContainer.getResourceBundle();
+        this.banFacade = banFacade;
     }
 
     @Override
-    public String call() {
-        CommandSender sender = context.getSender();
-        var senderUuid = PlaylegendBansPlugin.SERVER_UUID;
-
-        if(context.getPlayer() != null) {
-            senderUuid = context.getPlayer().getUniqueId();
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (!(sender instanceof ConsoleCommandSender) && !sender.hasPermission("playlegend.ban")) {
+            context.runInContext(sender, (ctxSender) -> {
+                ctxSender.sendMessage(LocalHelper.translate(this.resourceBundle, LocalKeys.NO_PERMISSION));
+                return null;
+            });
+            return true;
+        }
+        // /ban <player> <duration> <message>
+        if (args.length < 1) {
+            context.runInContext(sender, (ctxSender) -> {
+                ctxSender.sendMessage(LocalHelper.translate(this.resourceBundle, LocalKeys.NOT_ENOUGH_ARGS));
+                return null;
+            });
+            return false;
         }
 
-        final var maybeUser = this.userService.findById(senderUuid);
-        if (maybeUser.isEmpty()) {
-           return ChatColor.RED + "Something went wrong. Please relog";
+        var player = Bukkit.getPlayerExact(args[0]);
+        if (player == null) {
+            context.runInContext(sender, ContextProperties.of(LocalePlaceholderHelper.buildTargetPlayerContext(args[0])), (ctxSender, ctxProps) -> {
+                ctxSender.sendMessage(LocalHelper.translate(this.resourceBundle, context, LocalKeys.PLAYER_NOT_ONLINE));
+                return null;
+            });
+            //ChatColor.RED + "The mentioned player is not online: " + args[0]);
+            return true;
         }
 
-        final var user = maybeUser.get();
-        final var toBeBannedPlayer = Bukkit.getPlayerExact(player);
-        if(player == null || toBeBannedPlayer == null) {
-            return ChatColor.RED + "You must specify who you want to ban at index 0: Use /ban <player>";
+        var duration = "999y";
+        if (args.length >= 2) {
+            duration = args[1]; // duration
         }
-
-        final var maybeToBeBannedUser = this.userService.findById(toBeBannedPlayer.getUniqueId());
-        if(maybeToBeBannedUser.isEmpty()) {
-            return ChatColor.RED + "Something went wrong. The user you are trying to ban must relog";
-        }
-
-        final var toBeBannedUser = maybeToBeBannedUser.get();
 
         Date until;
+        try {
+            until = new DurationParser().parse(duration);
+        } catch (UnknownDurationUnitException e) {
+            context.runInContext(sender, ContextProperties.of(LocalePlaceholderHelper.buildAllowedUnitsContext()), (ctxSender, ctxProps) -> {
+                ctxSender.sendMessage(LocalHelper.translate(this.resourceBundle, context, LocalKeys.INVALID_DURATION_UNIT));
+                return null;
+            });
+            log.log(Level.SEVERE, "An unknown unit has been provided", e);
+            return true;
+        }
+
+        var message = LocalHelper.translate(this.resourceBundle, LocalKeys.DEFAULT_BAN_REASON);
+
+        if(args.length >= 3) {
+            message = String.join(" ", Arrays.copyOfRange(args, 2, args.length));
+        }
 
         try {
-            until = new DurationParser().parse(this.duration);
-        } catch (UnknownDurationUnitException ex) {
-            return "Wrong unit as duration provided. Allowed units are: " + String.join(", ", DurationPossibilities.getAllowedIdentifiers());
-        }
-        final var ban = Ban.builder()
-                .banned(toBeBannedUser)
-                .bannedBy(user)
-                .reason(this.message)
-                .until(until)
-                .active(true)
-                .build();
+            var ban = this.banFacade.banPlayer(player, sender, until, message);
+            context.runInContext(sender, ContextProperties.of(LocalePlaceholderHelper.buildBanContext(ban)), (ctxSender, ctxProps) -> {
+                player.kickPlayer(LocalHelper.translate(this.resourceBundle, context, LocalKeys.BAN_MESSAGE));
+                return null;
+            });
 
-        toBeBannedPlayer.kickPlayer("");
-        this.banService.save(ban);
-        return ChatColor.GREEN + "Successfully banned player " + player;
+            return true;
+        } catch (SQLException e) {
+            sender.sendMessage(LocalHelper.translate(this.resourceBundle, context, LocalKeys.PLAYER_BANNED_ERROR));//"There was an error whilst banning the player " + player.getName());
+            log.severe("There was an error whilst banning a player");
+        }
+
+        return false;
     }
 }
